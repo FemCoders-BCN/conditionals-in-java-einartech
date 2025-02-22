9195a63ac0651b85dd4cd4a78f53dba0fb0fc82b
einartech <imalovelyghost13@gmail.com>
2025-02-22 17:16:49 +0100

    TrafficLights file working properly

--- TrafficLights.java
+++ TrafficLights.java
@@ -1,15 +1,28 @@
 public class TrafficLights {
     public static void main(String[] args) {
 
-        //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
-        
-        //Si es verde imprime en la terminal: Puedes seguir
-        //Si es amarillo imprime en la terminal: Ten cuidado
-        //Si es rojo imprime en la terminal: ¡¡¡Para!!!
-        //Si es otro color imprime en la terminal: El semáforo está dañado
+        // Escribe un programa que basado en el color del semáforo verde, amarillo o
+        // rojo determine las acciones de un conductor
 
-        //Cambia el valor de la variable, corre el programa y mira el resultado 
+        String trafficLightColor = "black";
+
+        // Si es verde imprime en la terminal: Puedes seguir
+        if (trafficLightColor.equals("green")) {
+            System.out.println("Puedes seguir");
+
+            // Si es amarillo imprime en la terminal: Ten cuidado
+        } else if (trafficLightColor.equals("yellow")) {
+            System.out.println("Ten cuidado");
+
+            // Si es rojo imprime en la terminal: ¡¡¡Para!!!
+        } else if (trafficLightColor.equals("red")) {
+            System.out.println("¡¡¡Para!!!");
+            // Si es otro color imprime en la terminal: El semáforo está dañado
+        } else {
+            System.out.println("El semáforo está dañado");
+        }
+        // Cambia el valor de la variable, corre el programa y mira el resultado
 
     }
-    
+
 }
